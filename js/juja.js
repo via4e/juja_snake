558baa5fd3c2a4de856793e8cb4f7789b1650c05
Савельев Вячеслав Валерьевич <vsavelev@i-novus.ru>
2017-10-18 18:07:14 +0300

    snake obj

--- js/juja.js
+++ js/juja.js
@@ -1,5 +1,13 @@
 function jujaStart () {
     console.log( 'juja' );
+    let snake={};
+    // 0 < x 29,  0 < y < 39
+    // snake.direction 0,1,2,3; 0-up, 1-down, 2-left, 3-right
+
+    snake.x = 15;
+    snake.y = 19;
+    snake.direction = 3;
+
     setInterval( loop, 1000)
 }
 
